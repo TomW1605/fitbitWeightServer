14872cd72e00a805e176f6ec5da076bebc311557
Thomas White <TomW1605@users.noreply.github.com>
2024-04-03 11:46:59 +0800

    convert to fast api

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,5 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-ENTRYPOINT ["python", "-m", "flask", "run", "--host=0.0.0.0"]
+#ENTRYPOINT ["python", "-m", "flask", "run", "--host=0.0.0.0"]
+ENTRYPOINT ["python", "-m", "uvicorn", "app:app", "--host", "0.0.0.0", "--port", "5000"]
--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 import secrets
 from typing import Optional
 
-from flask import Flask, request, redirect, session, url_for
 import requests
 import datetime
 import base64
 from pydantic import Field, SecretStr
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import RedirectResponse
+from fastapi.requests import Request
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env')
+
     client_id: SecretStr
     client_secret: SecretStr
 
     authorization_url: str = Field(default='https://www.fitbit.com/oauth2/authorize')
-    access_token_url: str = Field(default='https://www.fitbit.com/oauth2/token')
+    token_url: str = Field(default='https://www.fitbit.com/oauth2/token')
     weight_log_url: str = Field(default='https://api.fitbit.com/1/user/-/body/log/weight.json')
 
     base_url: str = Field(default='https://localhost:5000/')
@@ -29,20 +33,20 @@ if config.base_url.endswith('/'):
 if config.redirect_url is None:
     config.redirect_url = config.base_url + '/callback'
 
-app = Flask(__name__)
-if config.base_url.count('/') > 2:
-    app.config['SCRIPT_NAME'] = config.base_url.split('/', 3)[-1]
-    app.config['APPLICATION_ROOT'] = config.base_url.split('/', 3)[-1]
+app = FastAPI(root_path="/"+config.base_url.split('/', 3)[-1])
+# if config.base_url.count('/') > 2:
+#     app.config['SCRIPT_NAME'] = config.base_url.split('/', 3)[-1]
+#     app.config['APPLICATION_ROOT'] = config.base_url.split('/', 3)[-1]
 
 # Dictionary to store user keys and Fitbit access tokens
 user_keys = {}
 
-@app.route('/')
-def home():
-    return redirect(url_for('login', _external=True))
+@app.get("/")
+async def home():
+    return RedirectResponse(url=f"{config.base_url}/login")
 
-@app.route('/login')
-def login():
+@app.get("/login")
+async def login():
     # Generate a unique key for the user
     user_key = secrets.token_urlsafe(16)
 
@@ -50,14 +54,13 @@ def login():
     session_key = secrets.token_urlsafe(16)
     user_keys[session_key] = {'access_token': None}
 
-
     authorization_redirect_url = f"{config.authorization_url}?client_id={config.client_id.get_secret_value()}&redirect_uri={config.redirect_url}&response_type=code&scope=weight&state={session_key}"
-    return redirect(authorization_redirect_url)
+    return RedirectResponse(url=authorization_redirect_url)
 
-@app.route('/callback')
-def callback():
-    authorization_code = request.args.get('code')
-    session_key = request.args.get('state')
+@app.get("/callback")
+async def callback(request: Request):
+    authorization_code = request.query_params.get('code')
+    session_key = request.query_params.get('state')
 
     data = {
         'client_id': config.client_id.get_secret_value(),
@@ -71,19 +74,23 @@ def callback():
         'Authorization': 'Basic ' + base64.b64encode(f"{config.client_id.get_secret_value()}:{config.client_secret.get_secret_value()}".encode()).decode()
     }
 
-    response = requests.post(config.access_token_url, headers=headers, data=data)
+    response = requests.post(config.token_url, headers=headers, data=data)
+
+    print(response.json())
 
     if response.status_code == 200:
         access_token = response.json()['access_token']
         user_keys[session_key]['access_token'] = access_token
         return f"Logged in! Your user key is: {session_key}"
     else:
-        return 'Error getting access token'
+        return response.json()
+        # raise HTTPException(status_code=400, detail='Error getting access token')
 
-@app.route('/add_weight', methods=['POST'])
-def add_weight():
-    user_key = request.form.get('user_key')
-    weight = float(request.form.get('weight'))
+@app.post("/add_weight")
+async def add_weight(request: Request):
+    form_data = await request.form()
+    user_key = form_data.get('user_key')
+    weight = float(form_data.get('weight'))
 
     # Check if the user key exists and has an associated access token
     if user_key in user_keys and user_keys[user_key]['access_token']:
@@ -104,9 +111,10 @@ def add_weight():
         if response.status_code == 201:
             return 'Weight data added successfully!'
         else:
-            return 'Error adding weight data'
+            raise HTTPException(status_code=400, detail='Error adding weight data')
     else:
-        return 'Unauthorized'
+        raise HTTPException(status_code=401, detail='Unauthorized')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
